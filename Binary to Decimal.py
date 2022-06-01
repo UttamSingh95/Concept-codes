41ce59b8753e98e7f698f30b8d46db2400527339
Uttam Singh <106603117+UttamSingh95@users.noreply.github.com>
2022-06-01 13:22:13 +0530

    Update Binary to Decimal.py

--- Binary to Decimal.py
+++ Binary to Decimal.py
@@ -8,24 +8,15 @@ You are given a number A in binary format (Base = 2). You have to print the numb
 explaination link: https://www.rapidtables.com/convert/number/binary-to-decimal.html
 """
 
-def main():
-    # YOUR CODE GOES HERE
-    # Please take input and print output to standard input/output (stdin/stdout)
-    # E.g. 'input()/raw_input()' for input & 'print' for output
-    def decimal_converter():
-        num = input()
 
-        ans = 0
-        place = len(num)
-        for x in num:
-            ans = ans + (int(x) * (2 ** (place - 1)))
-            place -= 1
-        print(ans)
+def decimal_converter():
+    num = input()
 
-    decimal_converter()
+    ans = 0
+    place = len(num)
+    for x in num:
+        ans = ans + (int(x) * (2 ** (place - 1)))
+        place -= 1
+    print(ans)
 
-
-    return 0
-
-if __name__ == '__main__':
-    main()
+decimal_converter()
